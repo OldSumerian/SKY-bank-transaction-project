17a15160fa82240d32d07e21bc1fb1cddd5e221a
Sumerian <shemerov.s@yandex.ru>
2024-03-11 20:07:57 +0300

    add func_read_json

--- util/main.py
+++ util/main.py
@@ -1,2 +1,17 @@
 # Курсовая работа №3 (основы Backend-разработки)
 # выполнена Щемеровым С.А. (профессия "Backend-разработчик" 38IND)
+
+import json
+
+
+def get_info_from_json():
+    """
+    Получает данные из json файла
+    :return:
+    """
+    with open('operations.json') as oper:
+        oper_full_info = json.loads(oper.read())
+        return oper_full_info
+
+
+print(get_info_from_json())
